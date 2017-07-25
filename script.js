9d2277de246a17292bf519d23a35c9f52b748a12
Noldy PC <arnodhis@gmail.com>
2017-07-25 08:36:19 +0300

    Objects exercise completed

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var box = {
-    material: 'cardboard',
-    name: 9,
-    year: 2017,
-    key: "check"
-};
+// https://github.com/bgando/object-exercises/
+
+// Objects exercise
+
+const ANIMAL = {};
+ANIMAL.username = "piggy";
+
+ANIMAL['tagline'] = "eat all you can";
+
+const NOISES = [];
+ANIMAL['noises'] = NOISES;
+
+let counter = 1;
+for (let prop in ANIMAL) {
+    console.log(prop);
+    if (prop === 'username') {
+        console.log("Hi my name is " + ANIMAL.username);
+
+    }
+    if (prop === 'tagline') {
+        console.log("I like to say " + ANIMAL.tagline);
+    }
+
+    counter++;
+}
+console.log("The for in loop was looped " + counter + ' times');
 
